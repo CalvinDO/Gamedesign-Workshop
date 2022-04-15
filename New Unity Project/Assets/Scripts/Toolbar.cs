b2b419600bd11e84155c9f4b67f52f0dbc20ae31
Jonathan Schnee <jonathan.schnee@hs-furtwangen.de>
2022-04-15 19:52:14 +0200

    feature(toolbar): choosing the form in a toolbar implemented

---
+++ New Unity Project/Assets/Scripts/Toolbar.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toolbar : MonoBehaviour
+{
+    public GameObject formListGO;
+    public GameObject active;
+    public CharacterController player;
+    public GameObject attackForms;
+    private List<GameObject> formList = new List<GameObject>();
+    private List<GameObject> attackFormList = new List<GameObject>();
+    private int formidx = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in formListGO.transform)
+        {
+            formList.Add(child.gameObject);
+        }
+        foreach (Transform child in attackForms.transform)
+        {
+            attackFormList.Add(child.gameObject);
+        }
+        attackFormList[formidx].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0)
+        {
+            if (scroll > 0)
+            {
+                attackFormList[formidx].SetActive(false);
+                formidx--;
+                if (formidx < 0)
+                {
+                    formidx = formList.Count - 1;
+                }
+                attackFormList[formidx].SetActive(true);
+            }
+            else
+            {
+                attackFormList[formidx].SetActive(false);
+                formidx++;
+                if (formidx > formList.Count - 1)
+                {
+                    formidx = 0;
+                }
+                attackFormList[formidx].SetActive(true);
+            }
+            active.transform.position = formList[formidx].transform.position;
+
+        }
+    }
+}
