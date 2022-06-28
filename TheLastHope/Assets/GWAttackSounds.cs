8b6dcf9b535484d6ea27e713a8c34f51c29c9ced
Calvin <calvindelloro@mail.de>
2022-06-28 17:32:32 +0200

    feature(Sound): first spell sound implemented

--- TheLastHope/Assets/GWAttackSounds.cs
+++ TheLastHope/Assets/GWAttackSounds.cs
@@ -6,6 +6,14 @@ public class GWAttackSounds : MonoBehaviour
 {
     public AudioSource audioSource;
 
+    public AudioClip throwClipBuilup;
+    public AudioClip shootClipBuildup;
+    public AudioClip stompClipBuildup;
+
+    public AudioClip throwClipSummon;
+    public AudioClip shootClipSummon;
+    public AudioClip stompClipSummon;
+
     void Start()
     {
         
@@ -18,26 +26,41 @@ public class GWAttackSounds : MonoBehaviour
     }
 
     public void BuildupStomp() {
-
+        this.audioSource.Stop();
+        this.audioSource.PlayOneShot(this.stompClipBuildup);
     }
 
     public void BuildupShoot() {
+        this.audioSource.Stop();
+
+        this.audioSource.PlayOneShot(this.shootClipBuildup);
 
     }
 
     public void BuildupThrow() {
+        this.audioSource.Stop();
+
+        this.audioSource.PlayOneShot(this.throwClipBuilup);
 
     }
 
     public void SummonStomp() {
+        this.audioSource.Stop();
+
+        this.audioSource.PlayOneShot(this.stompClipSummon);
 
     }
 
     public void SummonShoot() {
+        this.audioSource.Stop();
 
+        this.audioSource.PlayOneShot(this.shootClipSummon);
     }
 
     public void SummonThrow() {
+        this.audioSource.Stop();
+
+        this.audioSource.PlayOneShot(this.throwClipSummon);
 
     }
 }
