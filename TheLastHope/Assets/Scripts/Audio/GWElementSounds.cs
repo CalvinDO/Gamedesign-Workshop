b437d0190a014503fec57ada7863f70a15e6e85b
Johann Schulenburg <56830175+JohannSchulenburg@users.noreply.github.com>
2022-07-04 10:41:44 +0200

    Implement Element Tier 2 Sounds

--- TheLastHope/Assets/Scripts/Audio/GWElementSounds.cs
+++ TheLastHope/Assets/Scripts/Audio/GWElementSounds.cs
@@ -8,6 +8,12 @@ public class GWElementSounds : MonoBehaviour
     public AudioClip[] waterClips;
     public AudioClip[] earthClips;
     public AudioClip[] airClips;
+    public AudioClip[] sandClips;
+    public AudioClip[] ironClips;
+    public AudioClip[] plantClips;
+    public AudioClip[] lightningClips;
+    public AudioClip[] steamClips;
+    public AudioClip[] iceClips;
     public static GWElementSounds instance;
 
     void Awake() {
@@ -29,6 +35,24 @@ public class GWElementSounds : MonoBehaviour
                 return;
             case GWEType.AIR:
                 GWAudioManager.PlayClip(source, airClips[phase]);
+                return;
+            case GWEType.SAND:
+                GWAudioManager.PlayClip(source, sandClips[phase]);
+                return;
+            case GWEType.IRON:
+                GWAudioManager.PlayClip(source, ironClips[phase]);
+                return;
+            case GWEType.PLANT:
+                GWAudioManager.PlayClip(source, plantClips[phase]);
+                return;
+            case GWEType.LIGHTNING:
+                GWAudioManager.PlayClip(source, lightningClips[phase]);
+                return;
+            case GWEType.STEAM:
+                GWAudioManager.PlayClip(source, steamClips[phase]);
+                return;
+            case GWEType.ICE:
+                GWAudioManager.PlayClip(source, iceClips[phase]);
                 return;
             default:
                 return;
