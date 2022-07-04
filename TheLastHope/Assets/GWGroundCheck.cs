3275139e76cef5b9ee10c19d796b2bcfcb314da7
Calvin <calvindelloro@mail.de>
2022-07-04 23:28:37 +0200

    slide fix

--- TheLastHope/Assets/GWGroundCheck.cs
+++ TheLastHope/Assets/GWGroundCheck.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GWGroundCheck : MonoBehaviour
-{
+public class GWGroundCheck : MonoBehaviour {
     public GWEnemyController enemyController;
 
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         Ray ray = new Ray(this.transform.position, Vector3.down);
         RaycastHit hit;
 
@@ -18,11 +16,11 @@ public class GWGroundCheck : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, 0.1f, layerMask)) {
             this.enemyController.isGrounded = true;
-            
+            this.enemyController.transform.position = new Vector3(this.enemyController.transform.position.x, 0, this.enemyController.transform.position.z);
+
         }
         else {
             this.enemyController.isGrounded = false;
-            this.enemyController.transform.position = new Vector3(this.enemyController.transform.position.x, 0, this.enemyController.transform.position.z)
         }
     }
 }
